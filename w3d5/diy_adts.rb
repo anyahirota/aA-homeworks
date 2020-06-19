2db9e6cf8a176412d73b636c4db98ebbcd538053
anyahirota <anyahirota@gmail.com>
2020-06-18 21:38:45 -0700

    Create Queue class

--- w3d5/diy_adts.rb
+++ w3d5/diy_adts.rb
@@ -19,3 +19,21 @@ class Stack
       @stack[-1]
     end
   end
+
+  class Queue
+    def initialize
+      @queue = []
+    end
+
+    def enqueue(el)
+      @queue.unshift(el)
+    end
+
+    def dequeue
+      @queue.pop
+    end
+
+    def peek
+      @queue[-1]
+    end
+  end
